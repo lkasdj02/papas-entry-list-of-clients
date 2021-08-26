eabedfd132d565b2b965052e70bcc5cf83b38378
Zeo <zeo@iMac-di-Zeo.fritz.box>
2021-08-26 16:00:43 +0200

    added debug log

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const http = require("http");
+const { hostname } = require("os");
 const PORT = 3000;
 const server = http.createServer();
 
-server.listen(PORT, () => {
-  console.log("listening on port 3000...");
-});
 server.on("request", (request, response) => {
   const { method, url } = request;
   if (method === "GET" && url === "/") {
+    console.log(`${method} request was mad to ${url} url...`);
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json");
     response.write(JSON.stringify({ data: "hello world" }));
     response.end();
   }
+});
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
