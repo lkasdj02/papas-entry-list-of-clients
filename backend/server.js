901ddf66b1d3eb389dc24187ccaa87aee48a08c2
Zeo <leonardo.basso02@gmail.com>
2021-08-30 16:27:16 +0200

    added regexp pattern matching for user id on the delete functionality

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,17 @@ server.on("request", (request, response) => {
     sendFileBack("../res/index.html", response);
   } else if (method === "POST" && url === "/createuser") {
     insertUser(request, response);
-  } else if (method === "POST" && url.match("/deleteuser/[0-9]+")) {
-    // delete user functionality
+  } else if (method === "DELETE" && url.match(/\/deleteuser\/\d+/g)) {
+    console.log("mathches the url: " + url);
+    let id = url.match(/\d+/g);
+    deleteUser(request, response, id);
   } else if (method === "POST" && url === "/createentry") {
     insertEntry(request, response);
+  } else {
+    response.statusCode = 404;
+    response.writeHead("Content-type", "application/json");
+    response.write({ data: "resource not found" });
+    response.end();
   }
 });
 
