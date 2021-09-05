df24502ad25b642251a2fbf3d828fa39ba9b24d2
Zeo <leonardo.basso02@gmail.com>
2021-09-05 17:31:26 +0200

    added fetch feature for requesting stuff from the html page

--- res/main.js
+++ res/main.js
@@ -1,13 +1,29 @@
 // HTML elements
 const txtUserUuid = document.getElementById("insertuser-uuid");
-
+const sendUserData = document.getElementById("getUserById");
+const containerDiv = document.getElementById("container");
 // EVENT handlers
 sendUserData.addEventListener("click", () => {
   console.log("user clicked the button");
-  let userId = txtUserUuid.innerText;
-  fetch("http://localhost:3000/finduser/1234").then((item) =>
-    console.log(item)
-  );
+  try {
+    let userId = txtUserUuid.value === null ? "000" : txtUserUuid.value;
+    console.log(userId);
+    fetch(`http://localhost:3000/finduser/${userId}`)
+      .then((res) => {
+        let p2 = res.json();
+        return p2;
+      })
+      .then((item) => {
+        let node = document.createElement("h1"); // Create a <li> node
+        let textnode = document.createTextNode(item.name); // Create a text node
+        node.appendChild(textnode);
+        containerDiv.appendChild(node);
+        console.log(item);
+      })
+      .catch((err) => console.log(err));
+  } catch (error) {
+    console.log(`something went wrong ${error}`);
+  }
 });
 
 // functions
