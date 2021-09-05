d11abad708866c3995e504d606e7ecab9aca76d9
Zeo <leonardo.basso02@gmail.com>
2021-09-05 17:06:47 +0200

    simplified html for testing purposes

--- res/main.js
+++ res/main.js
@@ -1,11 +1,13 @@
 // HTML elements
-const sendUserData = document.getElementById("sendUserData");
-const txtUserName = document.getElementById("insertuser-name");
-const txtUserSurname = document.getElementById("insertuser-surname");
 const txtUserUuid = document.getElementById("insertuser-uuid");
+
 // EVENT handlers
 sendUserData.addEventListener("click", () => {
   console.log("user clicked the button");
+  let userId = txtUserUuid.innerText;
+  fetch("http://localhost:3000/finduser/1234").then((item) =>
+    console.log(item)
+  );
 });
 
 // functions
